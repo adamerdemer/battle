702b215af42a4437303d4f2a339675c95aa22d0a
Alex Chalk <alexchalk17@gmail.com>
2017-04-26 10:09:19 -0400

    Players can enter names

--- spec/features/infrastructure_test_spec.rb
+++ spec/features/infrastructure_test_spec.rb
@@ -1,7 +1,9 @@
-feature "Homepage" do
-  scenario 'visited homepage' do 
+feature "Battle" do
+  scenario 'filling in forms' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in "player_1", with: 'Alex'
+    fill_in "player_2", with: 'Adam'
+    click_button("Let's Play!")
+    expect(page).to have_content('The Epic Battle of Alex and Adam')
   end
 end
-
