562f29851bbb57e6e3761d3685fff0f8ad7e761d
Paula <pmuldoon@umich.edu>
2017-04-27 12:32:02 +0100

    12:31 role swap

--- app.rb
+++ app.rb
@@ -1,16 +1,20 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
 
   get '/' do
-    'Testing infrastructure working!'
     erb :index
   end
 
   post '/names' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
-    erb :names
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    erb :play
   end
 
   run! if app_file == $0
