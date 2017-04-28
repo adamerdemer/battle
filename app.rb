19bb6a4227a54c70124dd0a3cb5f1dee9732347d
Adam Erdem Er <adamerdemer@gmail.com>
2017-04-28 15:10:32 +0100

    Added stuff

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-
+require './lib/player'
 require 'sinatra/base'
 #require 'sinatra/reloader' if development?
 
@@ -12,20 +12,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1])
-    $player_2 = Player.new(params[:player_2])
+  $player_1 = Player.new(params[:player_1])
+  $player_2 = Player.new(params[:player_2])
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = $player_1.name
-    @player_2 = $player_2.name
+    @player_1 = $player_1
+    @player_2 = $player_2
     erb :play
   end
 
   get '/attack' do
-    @player_1 = $player_1.name
-    @player_2 = $player_2.name
+    @player_1 = $player_1
+    @player_2 = $player_2
     erb :attack
   end
 
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,6 +1,6 @@
 feature 'View hit points' do
   scenario 'see Player 2 hit points' do
   sign_in_and_play
-    expect(page).to have_content 'Adam: 60HP'
+    expect(page).to have_content 'Adam has 60 hitpoints'
   end
 end
