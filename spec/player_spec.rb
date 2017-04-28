aafa9f8315ddc16a3789a8f1e63d7d7234b1f513
Adam Erdem Er <adamerdemer@gmail.com>
2017-04-28 14:27:25 +0100

    Added player damage tests and updated class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,7 +2,7 @@ require 'player'
 
 describe Player do
   subject(:alex) { Player.new('Alex') }
-
+  subject(:adam) { Player.new('Adam') }
   describe '#name' do
     it 'returns the name' do
       expect(alex.name).to eq 'Alex'
@@ -14,4 +14,12 @@ describe Player do
     it 'returns the hit points' do
       expect(alex.hit_points).to eq described_class::DEFAULT_HIT_POINTS
     end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(adam).to receive(:receive_damage)
+      alex.attack(adam)
+    end
+  end
+
 end
